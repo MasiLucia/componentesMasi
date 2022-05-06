27fa122a1d4b75575dbcc05978c6b616129d7c09
Lucia Masi <masilucia11@gmail.com>
2022-05-06 11:07:52 -0300

    h

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContentComponent } from './content/content.component';
+import { HeaderComponent } from './header/header.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContentComponent,
+    HeaderComponent,
+    SidemenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
